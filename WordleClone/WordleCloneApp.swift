4f821eaa0b660bafb7a87c25cf4de588068234b6
Manan Patel <manan@wattpad.com>
2022-02-16 18:21:51 -0500

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Manan Patel on 2022-02-16.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
